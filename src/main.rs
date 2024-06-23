3999d7fe50b48cc67a11b1fd37638bb67236436d
fokot <frantisek.kocun@gmail.com>
2024-06-23 21:32:37 +0200

    more general scraper

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,39 @@
+use std::collections::LinkedList;
+use std::fs::File;
 use scraper::{Html, Selector};
-
-
-
+use std::io::Write;
+use std::iter::Map;
 
 fn main() {
-    let url = "https://books.toscrape.com/";
-    let response = reqwest::blocking::get(url).expect("Could not load url.");
+
+    // get current time
+    // let now = time::Instant::now();
+
+    let pages = LinkedList::new();
+
+    let client = reqwest::blocking::Client::builder().cookie_store(true).build().unwrap();
+
+    let domain = "https://books.toscrape.com";
+    let root_page = "/";
+    let root_url = concat!(domain,root_page);
+    let root_file_name = "index.html";
+    let response = client
+        .get(root_url)
+        .send().expect("Could not load url.");
+
+    // // let response = reqwest::blocking::get(r).expect("Could not load url.");
     let body = response.text().unwrap();
+
+
     print!("{}",body);
+    let mut file = File::create(root_file_name).expect("Could not create file.");
+    file.write_all(body.as_bytes()).expect("Could not write to file.");
 
     let document = Html::parse_document(&body);
-
-    let book_selector = Selector::parse("article.product_pod").unwrap();
-    let book_name_selector = Selector::parse("h3 a").unwrap();
-    let book_price_selector = Selector::parse(".price_color").unwrap();
-    let book_link_selector = Selector::parse("h3 a").unwrap();
-    for element in document.select(&book_selector) {
-        let book_name_element = element.select(&book_name_selector).next().expect("Could not select book name.");
-        let book_name = book_name_element.value().attr("title").expect("Could not find title attribute.");
-        let price_element = element.select(&book_price_selector).next().expect("Could not find price");
-        let price = price_element.text().collect::<String>();
-        println!("{:?} - {:?}",book_name, price);
-
-        let book_link_element = element.select(&book_name_selector).next().expect("Could not find book link element.");
-        let book_link= book_link_element.value().attr("href").expect("Could not find href attribute");
+    let selector = Selector::parse("a").unwrap();
+    for element in document.select(&selector) {
+        let href = element.value().attr("href").expect("Could not find href attribute.");
+        println!("{}",href);
     }
 }
 
